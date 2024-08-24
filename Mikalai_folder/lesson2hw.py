7c72e7abf7c31adb3c543a4ae858c285e98dc59f
Machanski Mikalai <mochan@192.168.1.140>
2024-08-24 16:46:13 +0300

    mikalai hw2

---
+++ Mikalai_folder/lesson2hw.py
@@ -0,0 +1,41 @@
+#a,b
+a = 4
+b = 7 
+c = 1.5
+d = 2.5
+e = "str"
+f = bool
+g = [1,2,3]
+sum = (a+b+c+d)
+mpl = (a*b*c*d)
+div = (a/b/c/d)
+pwr = (a**b/c**d)
+sub = (a-b-c-d)
+print (sum, mpl, div, pwr, sub)
+#c
+coffee = 5 
+chocolate = 7 
+banana = 4 
+tomato = 2 
+total = (coffee + chocolate + banana + tomato)
+print (total)
+#d
+a = 5
+b = a + 3
+c = a + b
+print (a,b,c)
+#e
+a = "coffee"
+b = "chocolate" 
+c = "banana" 
+d = "tomato" 
+print ("I have :", a,b,c,d)
+
+
+
+
+
+
+
+
+
