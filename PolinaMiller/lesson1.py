8b70c86a397abf3024946934e32be81c69c47c27
polinamiller <millerpolina074@gmail.com>
2024-08-24 21:57:29 +0400

    added homework for lessons 1 and 2

--- PolinaMiller/lesson1.py
+++ PolinaMiller/lesson1.py
@@ -1,2 +1,58 @@
-a = 6
-print (a)
+# Задание 1
+example_1 = 6 #integer
+print(example_1)
+
+example_2 = '''I'm studying Python at the "Teach Me Skills" school. 
+I'm taking an automation course.''' #string
+print(example_2)
+
+example_3 = 5.4 #float
+print(example_3)
+
+my_bool = True #bool
+
+# Задание 2
+a = 25
+b = 45
+print(a+b) #addition 25 + 45 = 70
+
+x = 10
+y = 6.8
+print(x-y) #subtraction 10 - 6.8 = 3.2
+
+c = 9
+d = 14.6
+print(c*d) #multiplication 9 * 14.6 = 131.4
+
+h = 8
+g = 3.2
+print(h/g) #division 8 / 3.2 = 2.5
+
+k = 5
+p = 4
+print(k**p) #exponentiation 5**4 = 625
+
+#Задание 3
+bread = 1.40
+sandomi_cookies = 4.95
+natakhtari = 4.90
+sour_cream = 4.85
+total_amount = bread + sandomi_cookies + natakhtari + sour_cream
+print("Total amount", total_amount)
+
+#Задание 4
+var_1 = 5
+var_2 = var_1 + 3
+var_3 = var_1 + var_2
+print(var_1)
+print(var_2)
+print(var_3)
+
+#Задание 5
+version_1 = 'apple'
+version_2 = 'bread,'
+version_3 = 'sour cream,'
+version_4 = 'chocolate,'
+version_5 = 'ice cream'
+shopping_list = f"Today I bought: {version_1}, {version_2}, {version_3}, {version_4}, {version_5}"
+print(shopping_list)
