38fe50ffa9f7330c7a4075377d02af3417c546fb
Masha Badouskaya <mashabodowskaya@gmail.com>
2024-08-24 21:21:38 +0200

    recent homework added - a, b, c, d, e

---
+++ Masha_B_HomeWork/Homework_1-d.py
@@ -0,0 +1,11 @@
+"""
+Объявить три переменные типа int и присвоить первой числовое значение,
+вторая переменная равна первой переменной увеличенной на 3,
+а третья переменная равна сумме первых двух.
+"""
+
+number_1 = int(input("Enter a number: "))
+number_2 = number_1 * 3
+number_3 = number_1 + number_2
+
+print(f"The first number is {number_1},\nthe second number is {number_2}\nand the third number is {number_3}.")
---
+++ Masha_B_HomeWork/Homework_1-e.py
@@ -0,0 +1,11 @@
+"""
+Создайте переменные типа str в которых будут перечислены разные продукты питания,
+складывая строки сделайте фразу в которой будут перечисляться все продукты.
+Например “I have: chocolate_bar, coffee, tea”
+"""
+
+food_item_1 = "Frozen Pizza"
+food_item_2 = "Cold Diet Coke"
+food_item_3 = "M&Ms"
+
+print("I always have in my fridge: \n{}, {}, {}.".format(food_item_1, food_item_2, food_item_3))
