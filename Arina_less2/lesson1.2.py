85b73a73b499ac9800eb9260d48099d2cb1a982a
Aryna Lasminskaya <a.lasm9117@gmail.com>
2024-08-25 18:21:37 +0200

    home_wor1

---
+++ Arina_less2/lesson1.2.py
@@ -0,0 +1,18 @@
+ice_cream=15.99
+coffe= 27.33
+water=3
+
+sum=ice_cream+coffe+water
+print(sum)
+
+var1=3
+var2=var1*3
+var3=var1+var2
+var4="ice_cream"
+var5="cofee"
+var6="water"
+begin= "i have:"
+print(begin,var1, var4)
+print(begin, var2, var5)
+print (begin, var3, var6)
+print(begin,var1, var4,var2, var5, var3, var6 )
