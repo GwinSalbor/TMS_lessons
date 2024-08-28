a1d7aee4f5191a2b1b4bdfb4e4d4da2e4e170f1c
Lev Myasnikov <lev.myasnickoff@yandex.ru>
2024-08-28 16:33:29 +0300

    Добавил домашнее задание. Файл Homework1

---
+++ lev_folder/Homework1.py
@@ -0,0 +1,55 @@
+#Задание а
+test_int = 5
+test_float = 5.5
+test_str = "Lev Andreevich"
+test_bool = True
+
+#Задание b
+test_int += 5
+print(test_int)
+test_int *= 5
+print(test_int)
+test_int %= 3
+print(test_int)
+test_int **= 5
+print(test_int)
+test_int /= 5
+print(test_int)
+test_int -= 5
+print(test_int)
+
+test_float += 5
+print(test_float)
+test_float *= 5
+print(test_float)
+test_float %= 3
+print(test_float)
+test_float **= 5
+print(test_float)
+test_float /= 5
+print(test_float)
+
+#Задание c
+bubble_gum = 20
+lollypop = 15
+capucino = 40
+
+print("Cумма покупки: ",  bubble_gum + lollypop + capucino)
+
+#Задание d
+num1 = 10
+num2 = num1 + 3
+num3 = num1 + num2
+
+print(num3)
+
+#Задание e
+food1 = "chocolate_bar"
+food2 = "coffe"
+food3 = "tea"
+
+print("I have: " , food1, "," , food2 , "," , food3)
+
+
+
+
---
+++ lev_folder/lesson3.py
@@ -0,0 +1,69 @@
+# number1 = False
+# number2 = True 
+# number3 = 10
+
+# def logic_and(number1, number2):
+#     print("And: " , bool(number1 & number2))
+
+
+# def logic_or(number1, number2):
+#     print("Or: " , bool(number1 or number2))
+
+# def logic_xor(number1, number2):
+#     print("Xor: " , bool(number1 ^ number2))
+
+# logic_and(number1, number2)
+# logic_or(number1, number2)
+# logic_xor(number1, number2)
+
+# test1 = 10
+# test2 = 7
+
+# if test1 > test2:
+#     print("test1")
+# else:
+#     print("test2")
+
+# test1 = 10
+# test2 = 12
+
+# if test1 > test2:
+#     print("test1")
+# elif test1==10:
+#     print(10)
+# else:
+#     print("test2")
+
+# #циклы
+# #for i in range(1,10):
+#     #print(i)
+
+# for i in range(1,10):
+#     if i == 5:
+#         print("five")
+#     elif i % 2 == 1:
+#         print("odd")
+#     else:
+#         print(i)
+
+    #Типы данных
+    #int, str, float, bool, frozenset, tuple
+    #list, dict, set - изменяемые типы данных
+
+    #num_list = [1, 2, 3, 4, 5] либо можно написать  num_list = list(1, 2, 3, 4, 5)
+
+    #dict = {"key": 1}
+
+    #tuple_example = (1, 2, 4)
+
+first_list = [1, 2, 3, 4]
+first_set = {1, 2, 3, 4}
+first_dict = {"key": 1,  4: "asd"}
+
+print(first_dict["key"])
+
+# for i in first_list:
+#     print(i)
+    
+# for i in first_dict:
+#     print(i)
