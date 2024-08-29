3d85c42f95e675b2e1c00090c30083a5278b9d8c
korolionok <korolionok.ekaterina@ya.ru>
2024-08-29 10:09:26 +0300

    Домашка 1+2 урок

---
+++ Kate_Korolenok/HW_2_lesson.py
@@ -0,0 +1,59 @@
+#Задание 1.a
+a=5
+b=4.5
+c='test string'
+d=True
+
+#Задание 1.b
+a += 2
+print(a)
+
+a *= 3
+print(a)
+
+a %= 4
+print(a)
+
+a **= 5
+print(a)
+
+a /= 6
+print(a)
+
+a -= 7
+print(a)
+
+b += 9
+print(b)
+
+b *= 0.5
+print(b)
+
+b %= 4
+print(b)
+
+b **= 5.9
+print(b)
+
+b /= 4
+print(b)
+
+#Задание 1.c
+bulba= 95
+haladnik = 3.8
+garbata = 0.8
+
+print("Cумма покупки:",  bulba + haladnik + garbata)
+
+#Задание 1.d
+my_age = 30
+dima_age = my_age+3
+denis_age = my_age + dima_age
+
+
+#Задание 1.e
+prod1 = "цыбуля"
+prod2 = "цукар"
+prod3 = "цынамон"
+
+print("Купіць: ",prod1,",",prod2 ,",",prod3)
