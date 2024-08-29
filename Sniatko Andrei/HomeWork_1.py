66b75f71f56ed413947d178d871a0271f2d71b34
Andrei Sniatko <smitler14888@gmail.com>
2024-08-29 18:36:37 +0200

    HomeWork addeded

---
+++ Sniatko Andrei/HomeWork_1.py
@@ -0,0 +1,55 @@
+#1.a
+
+nf1 = 135    #int
+nf2 = 17.5   #float
+nf3 = "Hejka Andrzej" #srt
+nf4 = True #bool
+print("1.a:")
+print(type(nf1), nf1)
+print(type(nf2), nf2)
+print(type(nf3), nf3)
+print(type(nf4), nf4)
+
+#1.b
+
+num1 = nf1 + nf2
+num2 = nf1 - nf2
+num3 = nf1 * nf2
+num4 = nf1 / nf2
+num5 = nf1 ** nf2
+
+print("1.b:")
+print(num1)
+print(num2)
+print(num3)
+print(num4)
+print(num5)
+
+#1.c
+
+onion = 3.99
+salad = 2.99
+potato = 6.49
+mashrooms = 8.59
+beer = 5.49
+
+total_check = onion + salad + potato + mashrooms + beer
+
+print("1.c", total_check)
+
+#1.d
+
+f1 = 88
+f2 = f1 + 3
+f3 = f1 + f2
+print("1.d", f1,f2,f3)
+
+#1.e
+
+prod_1 = "onion"
+prod_2 = "potato"
+prod_3 = "salad"
+prod_4 = "mashrooms"
+
+print("1.e: I have: {prod_1}, {prod_2}, {prod_3},{prod_4}")
+
