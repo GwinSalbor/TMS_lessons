e4ae01b51777f09c719881f05f8e5b395d0ee9f8
Machanski Mikalai <mochan@MacBook-Pro-M.local>
2024-09-04 12:53:51 +0300

    Add hw4 without task 7

---
+++ Mikalai_folder/hw4.4.py
@@ -0,0 +1,7 @@
+list1 = ["Mikalai", "Machanski"]
+n = list1[0]
+s = list1[1]
+str1 = "Minsk"
+str2 = "Belarus"
+print(f"Привет,{n} {s}! Добро пожаоловать в {str1}, {str2}")
+
---
+++ Mikalai_folder/hw4.5.py
@@ -0,0 +1,6 @@
+list = ['I', 'love', 'arrays', 'they', 'are', 'my', 'favorite']
+str = ' '
+for el in list:
+     str +=el
+     str += ' '
+print(str)
