02505881bafae936e6a022bbb764dd9cb2245aa1
Aryna Lasminskaya <a.lasm9117@gmail.com>
2024-09-04 14:24:38 +0200

    home_work_lesson4

---
+++ Arina_less2/Arina_HW_l4.py
@@ -0,0 +1,60 @@
+# a=input()
+# if a == "hello":
+#     print("hello!")
+# elif a == "how are you?":
+#     print("good")
+# elif a.isdigit():
+#     print("i dont understand")
+# else:
+#     print("write something")
+# if a.startswith("hello"):
+#     print ("this is a greeting")
+# else: 
+#     print ("this is not a greeting")
+
+
+# a="Robin Singh"
+# b=a.split()
+# print(b)
+# c="I love arrays they are my favorite"
+# d=c.split()
+# print(d)
+
+
+# name=["Ivan, Ivanou"]
+# city= "Minsk"
+# country= "Belarus"
+# name = name[0].replace(",", "")  #извлеечение из списка
+# print("Привет",name + "!", "Добро пожаловать в", city, country + ".")
+
+
+# var1=['I', 'love', 'arrays', 'they', 'are', 'my', 'favorite']
+# var2=" ".join(var1)
+# print(var2)
+
+
+# spisok=[1,2,3,4,5,6,7,8,9,10]
+# spisok.insert(2, 22)
+# print (spisok)
+# spisok.pop(2)
+# print(spisok)
+
+
+# list=[]
+# for i in range (10):
+#     elem=input("введите элемент ")
+#     list.append(elem)
+# list.sort()
+# print (list)
+
+
+# def unik_num(var1):
+#     unik=0
+#     for num in var1:
+#         unik ^= num 
+#     return unik
+# var1=[1,5,2,9,2,9,1]
+# unik_num2=unik_num(var1)
+# print (unik_num2)
+
+
