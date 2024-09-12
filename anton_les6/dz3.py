2ac666b99fafdb8950409f1b8157065c9834a62b
Anton Antonov <antoha.dr.funker@gmail.com>
2024-09-12 11:46:29 +0300

    upd dz3

--- anton_les6/dz3.py
+++ anton_les6/dz3.py
@@ -11,49 +11,49 @@ random.choice() - и модуля string который я описал в по�
 
 import random, string, datetime
 
-list_of_allphabet = string.ascii_letters
+def generate_str_in_file(list_of_allphabet):
+    random_count_of_string = random.randint(2, 5)
 
-random_count_of_string = random.randint(2, 5)
-
-list_of_random_string = []
-list_of_big_string = []
-exit_list = []
-
-for k in range(random_count_of_string):
-    small_random_int = random.randint(3, 5)
-
-    for i in range(small_random_int):
-        big_random_int = random.randint(5, 15)
-
-        for j in range(big_random_int):
-            random_letters = random.choice(list_of_allphabet)
-            list_of_random_string.append(random_letters)
-
-        big_string = ''.join(list_of_random_string)
-        list_of_random_string = []
-        list_of_big_string.append(big_string)
-
-    value_random_int = random.randint(2, 5)
-
-    match value_random_int:
-        case 2:
-            list_of_big_string.append('200')
-        case 3:
-            list_of_big_string.append('300')
-        case 4:
-            list_of_big_string.append('400')
-        case 5:
-            list_of_big_string.append('500')
-
-    dt = str(datetime.datetime.now())
-
-    list_of_big_string.insert(0, str(dt[:-7]))
-
-    exit_list.append(' '.join(list_of_big_string))
+    list_of_random_string = []
     list_of_big_string = []
+    exit_list = []
 
-with open("hello.txt", "w") as my_file:
-    my_file.write('\n'.join(exit_list))
+    for k in range(random_count_of_string):
+        small_random_int = random.randint(3, 5)
+
+        for i in range(small_random_int):
+            big_random_int = random.randint(5, 15)
+
+            for j in range(big_random_int):
+                random_letters = random.choice(list_of_allphabet)
+                list_of_random_string.append(random_letters)
+
+            big_string = ''.join(list_of_random_string)
+            list_of_random_string = []
+            list_of_big_string.append(big_string)
+
+        value_random_int = random.randint(2, 5)
+
+        match value_random_int:
+            case 2:
+                list_of_big_string.append('200')
+            case 3:
+                list_of_big_string.append('300')
+            case 4:
+                list_of_big_string.append('400')
+            case 5:
+                list_of_big_string.append('500')
+
+        dt = str(datetime.datetime.now())
+
+        list_of_big_string.insert(0, str(dt[:-7]))
+
+        exit_list.append(' '.join(list_of_big_string))
+        list_of_big_string = []
+
+    with open("hello.txt", "w") as my_file:
+        my_file.write('\n'.join(exit_list))
 
 
-
+list_of_allphabet = string.ascii_letters
+generate_str_in_file(list_of_allphabet)
